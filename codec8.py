c314606012c5b3741823b06727bafd5da5a2c801
robertoCabreraAgro <roberto.cabrera@agromarin.mx>
2024-12-16 10:06:53 -0600

    fix ip

--- codec8.py
+++ codec8.py
@@ -8,7 +8,7 @@ import decimal
 #from pyproj import Transformer
 import threading  # Importamos threading para manejar múltiples hilos
 
-HOST = '34.229.87.213'  # Puede que '0.0.0.0' no funcione en algunos sistemas Linux; cambia a una cadena con la dirección IP, por ejemplo: '192.168.0.1'
+HOST = '0.0.0.0'  # Puede que '0.0.0.0' no funcione en algunos sistemas Linux; cambia a una cadena con la dirección IP, por ejemplo: '192.168.0.1'
 PORT = 5055  # Cambia esto por el puerto que estás utilizando
 
 # def conectar_bd():
